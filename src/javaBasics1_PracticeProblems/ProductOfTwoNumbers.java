208598e17a21bb6fc34ceecd738df152e3ef478b
Mohit Jaisingh <mohitmjaisingh@gmail.com>
2022-06-28 03:20:13 +0530

    changed ProductOfTwoNumbers

--- src/javaBasics1_PracticeProblems/ProductOfTwoNumbers.java
+++ src/javaBasics1_PracticeProblems/ProductOfTwoNumbers.java
@@ -1,17 +1,10 @@
 package javaBasics1_PracticeProblems;
-import java.util.Scanner;
+
 public class ProductOfTwoNumbers {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int a,b;
-		System.out.println("Enter 2 numbers ");
-		a = sc.nextInt();
-		b = sc.nextInt();
-		int prod = a*b;
-		
-		System.out.println("Product is " + prod);
+
 	}
 
 }
