9a80b4e379ed4ea21bad14a0600c1df731ab0a8f
Mohit Jaisingh <mohitmjaisingh@gmail.com>
2022-07-05 17:59:34 +0530

    added comments

--- src/arrays2_PracticeProblems/MinSumSubarray.java
+++ src/arrays2_PracticeProblems/MinSumSubarray.java
@@ -4,7 +4,7 @@ public class MinSumSubarray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = {-1,3,-6,-2,1,-3,-5,6,3,-5};
+		int a[] = {-1,3,-6,2,1,-3,-5,8,3,-5,-9,-9};
 		
 		System.out.println(minSumSubarray(a));
 	}
@@ -15,12 +15,12 @@ public class MinSumSubarray {
 		int sum = a[0];
 		
 		int from = 0, to = -1;
-		int finalFrom;
+		int finalFrom = from;
 		
 		for(int i=1; i<a.length; i++) {
 			if(minSum>sum) {
 				minSum = sum;
-				to = i-1;
+				to = i;
 				finalFrom = from;
 			}
 			
@@ -32,7 +32,7 @@ public class MinSumSubarray {
 			}
 		}
 		
-		System.out.println("from " + from + " index to " + to + " index");
+		System.out.println("from " + finalFrom + " index to " + to + " index");
 		return minSum;
 		
 	}
