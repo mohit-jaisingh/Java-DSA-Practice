531720e87affcb446be98b419521cf846959e2a0
Mohit Jaisingh <mohitmjaisingh@gmail.com>
2022-07-10 18:54:17 +0530

    added comments

--- src/arrays3_PracticeProblems/LargestSubarrayWithEqual0sAND1s.java
+++ src/arrays3_PracticeProblems/LargestSubarrayWithEqual0sAND1s.java
@@ -19,43 +19,45 @@ public class LargestSubarrayWithEqual0sAND1s {
 	
 		int[] prefixSum = new int[a.length];
 		
-		prefixSum[0] = a[0]==0? -1 : 1;
+		prefixSum[0] = a[0]==0? -1 : 1;  // set 0th prefixSum as -1 if a[0] is 0, else 1;
 		
-		for(int i=1; i<a.length; i++) {
+		for(int i=1; i<a.length; i++) { // continue filling prefix sum array
 			if(a[i]==0) prefixSum[i] += prefixSum[i-1]-1;
 			else prefixSum[i]+= prefixSum[i-1] + a[i];
 		}
 		
-		System.out.println("Prefix Sum");
-		for(int e: prefixSum) {
-			System.out.print(e + " ");
-		}
-		System.out.println();
+//		System.out.println("Prefix Sum");
+//		for(int e: prefixSum) {
+//			System.out.print(e + " ");
+//		}
+//		System.out.println();
 		
 		Map<Integer,Integer> occurance = new HashMap<>();
 		int maxLen = 0;
 		int startIndex = 0;
 		int endIndex = -1;
 		
-		for(int i=0; i<prefixSum.length; i++) {
+		for(int i=0; i<prefixSum.length; i++) { //store each first occuring unique prefixSum elements
 			
 			int curr = prefixSum[i];
-			if(curr == 0) {
-				maxLen = i+1;
+			
+			if(curr == 0) { // array uptil now has equal 0s and 1s.
+				maxLen = i+1; // It will be  the greatest maxLen uptil now.
 			}
 			
-			if(occurance.containsKey(curr)) {
-				if(maxLen < i-occurance.get(curr)) {
-					maxLen = i-occurance.get(curr);
+			if(occurance.containsKey(curr)) {  // if repeating element is found (sub array with equal 0s and 1s is found)
+				if(maxLen < i-occurance.get(curr)) {  // if the subarray's length is greater than maxLen
+					maxLen = i-occurance.get(curr); // update max len
 					endIndex = i;
 				}
 			}else {
-				occurance.put(curr, i);
+				occurance.put(curr, i); // add elements not present in PrefixSum along with index
+										// at which it was found.
 			}
 			
 		}
-		
-		System.out.println("index " + (endIndex-maxLen+1) + " to " + endIndex);
+		startIndex = endIndex-maxLen+1; //maxLen would be 1 lesser than actual maxLen
+		System.out.println("index " + startIndex + " to " + endIndex);
 		
 		return maxLen;
 	}
