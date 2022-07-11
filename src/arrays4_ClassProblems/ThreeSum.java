99aee150726abcd08f5a346ebf86f66d9402ab61
Mohit Jaisingh <mohitmjaisingh@gmail.com>
2022-07-12 01:20:00 +0530

    added code for Three Sum

---
+++ src/arrays4_ClassProblems/ThreeSum.java
@@ -0,0 +1,38 @@
+package arrays4_ClassProblems;
+
+import java.util.Arrays;
+
+public class ThreeSum extends TwoSum{ //find 3 numbers in an unsorted Array with given sum
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,2,4,7,3,5,-2,1};
+		int sum = 5;
+		
+		System.out.println(threeSum(a,sum));
+
+	}
+	
+	static boolean threeSum(int[] a, int sum) {
+		
+		Arrays.sort(a); // O(NlogN);
+		
+		for(int i=0; i<a.length; i++) {
+			if(TwoSum.findTwoSum(a, sum-a[i],i+1)) {
+				//System.out.println(" k = " + a[i]);
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	
+	
+	//Brute force is to have nested loop of 3 and check if that combo gives the sum.
+	
+	//optimized is to take ith element and then find TwoSum such that TwoSum + ith = sum.
+	
+	
+
+}
